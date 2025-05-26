ec3f080c0052562cbfb55e1f49c3de1d7d7337a4
Kavindu Prabhath Methpura <150885116+KavinduMethpura@users.noreply.github.com>
2025-05-26 19:46:40 +0530

    new instructions

--- Part3_Integration/cpuTestBench.v
+++ Part3_Integration/cpuTestBench.v
@@ -22,12 +22,29 @@ module cpu_tb;
         // Initialize instruction memory with the set of instructions you need execute on CPU
         
         // METHOD 1: manually loading instructions to instr_mem
-        //{instr_mem[10'd3], instr_mem[10'd2], instr_mem[10'd1], instr_mem[10'd0]} = 32'b00000000000001000000000000000101;
-        //{instr_mem[10'd7], instr_mem[10'd6], instr_mem[10'd5], instr_mem[10'd4]} = 32'b00000000000000100000000000001001;
-        //{instr_mem[10'd11], instr_mem[10'd10], instr_mem[10'd9], instr_mem[10'd8]} = 32'b00000010000001100000010000000010;
+        // loadi R4, 5
+        {instr_mem[3], instr_mem[2], instr_mem[1], instr_mem[0]} = 32'b00000000_00000100_00000000_00000101;
+
+        // loadi R2, 5
+        {instr_mem[7], instr_mem[6], instr_mem[5], instr_mem[4]} = 32'b00000000_00000010_00000000_00000101;
+
+        // add R6, R4, R2
+        {instr_mem[11], instr_mem[10], instr_mem[9], instr_mem[8]} = 32'b00000010_00000110_00000100_00000010;
+
+        // mov R1, R4
+        {instr_mem[15], instr_mem[14], instr_mem[13], instr_mem[12]} = 32'b00000001_00000001_00000100_00000000;
+
+        // sub R3, R6, R2
+        {instr_mem[19], instr_mem[18], instr_mem[17], instr_mem[16]} = 32'b00000011_00000011_00000110_00000010;
+
+        // and R7, R4, R2
+        {instr_mem[23], instr_mem[22], instr_mem[21], instr_mem[20]} = 32'b00000100_00000111_00000100_00000010;
+
+        // or R5, R4, R2
+        {instr_mem[27], instr_mem[26], instr_mem[25], instr_mem[24]} = 32'b00000101_00000101_00000100_00000010;
         
         // METHOD 2: loading instr_mem content from instr_mem.mem file
-        $readmemb("instr_mem.mem", instr_mem);
+        // $readmemb("instr_mem.mem", instr_mem);
     end
     
     /* 
